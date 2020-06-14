fc4f0b2ee678721565245d73ace77d5577e28b9a
Vitor Moraes <vitormoraes3@yahoo.com.br>
2020-06-14 17:44:32 -0300

    Feat: Frontend and Backend integration

--- backend/app/Controllers/Http/UserController.js
+++ backend/app/Controllers/Http/UserController.js
@@ -1,39 +1,67 @@
-'use strict'
+"use strict";
 
-const User = use('App/Models/User') //importando esse model
-const Database = use('Database');
+const User = use("App/Models/User"); //importando esse model
+const Database = use("Database");
+const Drive = use("Drive");
 
 class UserController {
-
-  async new({request, response}){
-    let data = request.only(['name','cpf','email','password'])
-    data['access'] = 1;
+  async new({ request, response }) {
+    let data = request.only(["name", "cpf", "email", "password"]);
+    data["access"] = 1;
+    const image = request.file("fileImage", {
+      types: ["image"],
+      size: "2mb",
+    });
+    if (image) {
+      await image.move("../public/uploads/profile-image");
+      if (!image.moved()) {
+        return image.error();
+      }
+      data["img_path"] = "/uploads/profile-image/" + image.fileName;
+    }
     const user = await User.create(data);
     return response.status(201).json(user);
   }
 
-  async login({request, response, auth}){
-    const data = request.only([/*'cpf'*/'email','password']) // let the user choose between email and cpf to use as login
-    const token = await auth.attempt(/*data.cpf*/ data.email, data.password);
+  async login({ request, response, auth }) {
+    const data = request.only([/*'cpf',*/ "email", "password"]); // let the user choose between email and cpf to use as login
+    const token = await auth.attempt(/*data.cpf,*/ data.email, data.password);
     return response.json(token);
   }
 
-  async getAll({response}){
+  async getAll({ response }) {
     const resp = await User.all();
     return response.json(resp);
   }
 
-  async get({response, params}){
+  async get({ response, params }) {
     const resp = await User.find(params.id);
     return response.json(resp);
   }
 
-  async edit({request, response, params}){
+  async edit({ request, response, params }) {
     let data = request.all();
-    const resp = await Database.table('users').where('id', params.id).update(data);
+    const image = request.file("fileImage", {
+      types: ["image"],
+      size: "2mb",
+    });
+    const oldFile = await User.find(params.id);
+    const exists = await Drive.exists("../../public" + oldFile.img_path);
+    if (image) {
+      if (exists) {
+        await Drive.delete("../../public" + oldFile.img_path);
+      }
+      await image.move("../public/uploads/profile-image/");
+      if (!image.moved()) {
+        return image.error();
+      }
+      data["img_path"] = "/uploads/profile-image/" + image.fileName;
+    }
+    const resp = await Database.table("users")
+      .where("id", params.id)
+      .update(data);
     return response.json(resp);
   }
-
 }
 
-module.exports = UserController
+module.exports = UserController;
--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,17 +1,30 @@
 import React, { useState } from "react";
+import api from "../services/api";
 
 export default function RegistrationPage() {
   const [name, setName] = useState(null);
   const [cpf, setCpf] = useState(null);
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
-  // Add perfil imagem
+  const [file, setFile] = useState(null);
+  const data = new FormData(); // Necessário sempre que for utilzar arquivos
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    data.append("name", name);
+    data.append("cpf", cpf);
+    data.append("email", email);
+    data.append("password", password);
+    data.append(
+      "fileImage",
+      new File([file], Date.now() + ".jpg", {
+        type: "image/jpg",
+      })
+    );
     try {
-      // Add api
-      //const response = await ...
-      //console.log(response);
+      await api.post("/users/new", data);
+      console.log("Cadastrado com sucesso");
+      window.location.href = "/home";
     } catch (error) {
       console.log(error);
     }
@@ -22,16 +35,16 @@ export default function RegistrationPage() {
     <div className="container">
       <div className="card mt-5">
         <div className="card-body">
-            <div classname="registration-header">
-              <div classname="text-center">
-                <h3>Cadatre-se abaixo!</h3>
-                <p>Não demora nem um minuto!</p>
-              </div>
+          <div classname="registration-header">
+            <div classname="text-center">
+              <h3>Cadastre-se abaixo!</h3>
+              <p>Não demora nem um minuto!</p>
             </div>
+          </div>
         </div>
       </div>
       {/* User information form */}
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
         <div className="container registration-body">
           <div className="form-group mt-5">
             <label htmlFor="name">Nome Completo</label>
@@ -50,8 +63,9 @@ export default function RegistrationPage() {
             {/* CPF's input */}
             <label htmlFor="cpf">CPF</label>
             <input
-              type="text" name="cpf" 
-              pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" 
+              type="text"
+              name="cpf"
+              pattern="\d{3}\.\d{3}\.\d{3}-\d{2}"
               required
               className="form-control"
               id="cpf"
@@ -88,17 +102,38 @@ export default function RegistrationPage() {
               }}
             />
             {/* add picture requisitation place */}
-            {/*  */}
-            {/*  */}
+            <label htmlFor="file">Foto de perfil</label>
+            <input
+              type="file"
+              required
+              className="form-control"
+              id="file"
+              name="file"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
           </div>
           {/* Button to register*/}
-          <div className="mx-auto col-12 col-md-6 text-center" style={{ maxWidth: 200 }}>
-            <button className="btn btn-custom waves-btn waves-effect mt-3">
+          <div
+            className="mx-auto col-12 col-md-6 text-center"
+            style={{ maxWidth: 200 }}
+          >
+            <button
+              className="btn btn-custom waves-btn waves-effect mt-3"
+              type="submit"
+            >
               Entrar
             </button>
           </div>
-          <div className="col-12 col-md-6 text-center" style={{ maxWidth: 200 }}>
-            <a className="btn btn-custom waves-btn waves-effect mt-3" href="/home">
+          <div
+            className="col-12 col-md-6 text-center"
+            style={{ maxWidth: 200 }}
+          >
+            <a
+              className="btn btn-custom waves-btn waves-effect mt-3"
+              href="/home"
+            >
               Voltar
             </a>
           </div>
